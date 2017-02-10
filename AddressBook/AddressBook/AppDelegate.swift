1f5578ad7fbd5d3abd11cada044df03344ef15c9
Gary Shirk <garyshirk@yahoo.com>
2017-02-10 14:19:55 -0600

    small fix from previous commit

--- AddressBook/AddressBook/AppDelegate.swift
+++ AddressBook/AddressBook/AppDelegate.swift
@@ -57,7 +57,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         
-        return false
+        // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded
+        return true
 //        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
 //        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
 //        if topAsDetailController.detailItem == nil {
